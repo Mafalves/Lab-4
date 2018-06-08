58055229c57d3ce9c8b5bf9b8f19b6dd7ae3acdd
Timoteo Farias Shockness <timoteo.ppt@gmail.com>
2018-06-07 23:30:59 -0700

    Agora vai

--- playground/db.js
+++ playground/db.js
@@ -1,3 +1,17 @@
 //Vamos aprender Banco de dados
 //MongoDb + Mongoose
 //mongodb://admin:123abc@ds016058.mlab.com:16058/dbtest -connection String
+
+//MongoDb, Cosmo, Firebase, Redis, Casandra    -      SQL, MySql, Oracle
+//NoSQL
+//Document Driven                              -  Entidades
+
+//SQL
+//Pessoa - Table, Id, Foreign key (nome - varchar, NOT NULL, sobrenome, varchar, NOT NULL, age, INT NULL);
+//Row
+
+
+//NoSQL
+//Pessoa - Collection, Id, 
+//Documents
+
