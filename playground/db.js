92cc51075b6a67b9d150bbc90e0e9aff3ec08333
Mafalves <mateus-farias@live.com>
2018-06-17 22:23:28 -0300

    Create - Analista Schema and Model

--- playground/db.js
+++ playground/db.js
@@ -10,7 +10,6 @@
 //Pessoa - Table, Id, Foreign key (nome - varchar, NOT NULL, sobrenome, varchar, NOT NULL, age, INT NULL);
 //Row
 
-
 //NoSQL
 //Pessoa - Collection, Id, 
 //Documents
@@ -18,14 +17,32 @@
 const db = require('mongoose');
 db.connect("mongodb://admin:123abc@ds016058.mlab.com:16058/dbtest");
 
-const Analista = db.model("Analista", {
-    name: String
+
+let analistaSchema = db.Schema({
+    firstName: String,
+    lastName: String
 });
 
-//const anal = new Analista({
-//    name: "Timão"
-//});
+let Analista = db.model('Analista', analistaSchema);
 
-//anal.save();
+/*
+let anl1 = new Analista(
+    {
+        firstName: 'Moisés',
+        lastName: 'Alves'
+    }
+);
 
-console.log(Analista.find({}));
+anl1.save(function(err, anl1){
+    if(err)
+        return console.error(err); 
+});
+*/
+
+Analista.find(function(err, anlistas){
+    if(err)
+    {
+        return console.error(err);
+    }
+    console.log(anlistas);
+});
